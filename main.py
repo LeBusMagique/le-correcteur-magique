d58aff03352fb2dc74901e4ae48c364eea2d8a5f
Anthony Destenay <anthony.destenay@gmail.com>
2021-10-09 11:55:20 +0200

    Replace file writing by postresql + added psycopg2 in requirement.txt + added runtime.txt + removed web in Profile + added readme.md

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
+import discord
+import psycopg2
 from dotenv import dotenv_values
 from trello import TrelloClient
-import discord
 from discord_slash.utils import manage_components
 from discord_slash.model import ButtonStyle
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from psycopg2.extras import RealDictCursor
 
 config = {
     **dotenv_values(".env"),
     **os.environ,
 }
+
 discord = discord.Client()
 scheduler = AsyncIOScheduler()
 
@@ -20,18 +23,22 @@ client = TrelloClient(
     token_secret='your-oauth-token-secret'
 )
 
+conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (config['DB_HOST'], config['DB_BASE'], config['DB_USER'], config['DB_PASS']))
+db = conn.cursor(cursor_factory=RealDictCursor)
+
 
 async def get_trello_cards_gw2():
     all_boards = client.list_boards()
     cards_new = 0
-    cards_ids_new = []
 
     channel = discord.get_channel(int(config['DISCORD_CHANNEL_GW2']))
-    filename = "trello_cards_gw2.txt"
 
-    with open(filename) as file:
-        cards_ids_old = file.read().splitlines()
-    file.close()
+    cards_ids_old = []
+    db.execute("SELECT * FROM items")
+    for item in db.fetchall():
+        cards_ids_old.append(item['uid'])
+
+    db.execute("DELETE FROM items")
 
     for board in all_boards:
         if board.id == config['TRELLO_BOARD_GW2']:
@@ -40,7 +47,7 @@ async def get_trello_cards_gw2():
             for trello_list in trello_lists:
                 if trello_list.id == config['TRELLO_LIST_TOPROOFREAD']:
                     for card in trello_list.list_cards():
-                        cards_ids_new.append(card.id)
+                        db.execute("INSERT INTO items (uid) VALUES ('%s')" % card.id)
 
                         if card.id not in cards_ids_old:
                             cards_new = cards_new + 1
@@ -65,12 +72,7 @@ async def get_trello_cards_gw2():
                             action_row = manage_components.create_actionrow(*buttons)
                             await channel.send(f':writing_hand: Nouvelle correction à faire : **{card.name}**', components=[action_row])
 
-    with open(filename, 'w+') as file:
-        file.truncate(0)
-        for card_id in cards_ids_new:
-            file.write("%s\n" % card_id)
-        file.close()
-
+    conn.commit()
     print(f'Nouvelles cartes ajoutées : {cards_new}')
 
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Le Correcteur Magique
+
+Un robot pour Discord qui envoie une notification lorsqu'une carte est déplacée dans la colonne "À relire" du Trello pour la gestion des contenus du Bus Magique ([lebusmagique.fr](https://www.lebusmagique.fr)).
+
+Réalisé par Thoanny
